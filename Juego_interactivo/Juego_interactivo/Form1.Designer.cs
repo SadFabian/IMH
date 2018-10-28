c120eed6878e7f919188163fce868b9b530271f3
Perla Arroyos <al164026@alumnos.uacj.mx>
2018-10-28 09:29:55 -0700

    Mi primer commit

---
+++ Juego_interactivo/Juego_interactivo/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Juego_interactivo
+{
+    partial class Pantalla_principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pantalla_principal));
+            this.image_logo = new System.Windows.Forms.PictureBox();
+            this.buttonRegistro = new System.Windows.Forms.Button();
+            this.buttonIngresa = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.image_logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image_logo
+            // 
+            this.image_logo.Image = ((System.Drawing.Image)(resources.GetObject("image_logo.Image")));
+            this.image_logo.Location = new System.Drawing.Point(286, 34);
+            this.image_logo.Name = "image_logo";
+            this.image_logo.Size = new System.Drawing.Size(253, 115);
+            this.image_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.image_logo.TabIndex = 0;
+            this.image_logo.TabStop = false;
+            this.image_logo.Click += new System.EventHandler(this.image_logo_Click);
+            // 
+            // buttonRegistro
+            // 
+            this.buttonRegistro.Location = new System.Drawing.Point(368, 201);
+            this.buttonRegistro.Name = "buttonRegistro";
+            this.buttonRegistro.Size = new System.Drawing.Size(75, 23);
+            this.buttonRegistro.TabIndex = 1;
+            this.buttonRegistro.Text = "button1";
+            this.buttonRegistro.UseVisualStyleBackColor = true;
+            this.buttonRegistro.Click += new System.EventHandler(this.buttonRegistro_Click);
+            // 
+            // buttonIngresa
+            // 
+            this.buttonIngresa.Location = new System.Drawing.Point(368, 298);
+            this.buttonIngresa.Name = "buttonIngresa";
+            this.buttonIngresa.Size = new System.Drawing.Size(75, 23);
+            this.buttonIngresa.TabIndex = 2;
+            this.buttonIngresa.Text = "button2";
+            this.buttonIngresa.UseVisualStyleBackColor = true;
+            this.buttonIngresa.Click += new System.EventHandler(this.buttonIngresa_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 447);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(590, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(210, 437);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(350, 256);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Pantalla_principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 442);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.image_logo);
+            this.Controls.Add(this.buttonIngresa);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.buttonRegistro);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Pantalla_principal";
+            this.Text = "GoPlay";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Pantalla_principal_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Pantalla_principal_FormClosed);
+            this.Load += new System.EventHandler(this.Pantalla_principal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.image_logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image_logo;
+        private System.Windows.Forms.Button buttonRegistro;
+        private System.Windows.Forms.Button buttonIngresa;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Juego_interactivo/Juego_interactivo/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego_interactivo
+{
+    public partial class Pantalla_principal : Form
+    {
+        public Pantalla_principal()
+        {
+            InitializeComponent();
+        }
+
+        private void image_logo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pantalla_principal_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle =FormBorderStyle.Fixed3D;
+            this.ControlBox = true;
+            
+            this.MinimizeBox = true;
+            this.MaximizeBox = false;
+
+            this.Text = "GOPLAY";
+            this.BackColor = Color.FromArgb(204, 255, 153);
+
+            this.buttonRegistro.Text = "Registrate";
+            this.buttonIngresa.Text = "Ingresar";
+           
+
+        }
+
+        private void buttonRegistro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonIngresa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void Pantalla_principal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Deseas cerrar PLAYGO?", "Mensaje del sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Pantalla_principal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+    }
+}
